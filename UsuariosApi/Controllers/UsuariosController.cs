eef8157f565bf4ee3bf446db34721b8df0a92e6e
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-03-08 22:53:08 -0300

    Add project files.

---
+++ UsuariosApi/Controllers/UsuariosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuariosController
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

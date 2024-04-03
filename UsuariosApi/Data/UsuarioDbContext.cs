9bd84713b8b9ef2288e1e3e70483d5206475939c
devfabiorm <fabio_evangelion_guitar@hotmail.com>
2024-04-02 22:11:53 -0300

    setting database up

---
+++ UsuariosApi/Data/UsuarioDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Data
+{
+    public class UsuarioDbContext : IdentityDbContext<Usuario>
+    {
+        public UsuarioDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected UsuarioDbContext()
+        {
+        }
+    }
+}

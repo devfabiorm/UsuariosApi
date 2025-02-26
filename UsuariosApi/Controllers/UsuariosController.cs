e0b571f7d46e7e15f96291365ad34c77eb566fdd
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-02-25 22:22:58 -0300

    feat: configured Identity to manage user

--- UsuariosApi/Controllers/UsuariosController.cs
+++ UsuariosApi/Controllers/UsuariosController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
 
 namespace UsuariosApi.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
-    public class UsuariosController
+    [ApiController]
+    public class UsuariosController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public UsuariosController(IMapper mapper, UserManager<Usuario> userManager)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+        {
+            Usuario usuario = _mapper.Map<Usuario>(dto);
+
+            var resultado = await _userManager.CreateAsync(usuario, dto.Password);
+
+            if (resultado.Succeeded)
+                return Ok("Usuário cadastrado!");
+
+            throw new ApplicationException("Falha ao cadastrar uauário");
         }
     }
 }
